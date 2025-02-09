f16660448bd8c5dd4ce6de0f5bd19c38d1f30eb1
Administrador <imperioinfinitus@gmail.com>
2025-02-09 12:39:35 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -64,7 +64,7 @@ app.post('/api/login', async (req, res) => {
             return res.status(400).json({ message: 'Senha incorreta' });
         }
 
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '5m' });
 
         res.cookie("token", token, 
             { httpOnly: true },
